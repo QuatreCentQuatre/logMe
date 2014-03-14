7e6ad7c450b36c23c7398e93dfff242c36085d97
Samuel Cléroux-Bouthillier <samuel@scbfolio.com>
2014-03-14 17:53:04 -0400

    add a delay in mobile log

--- src/logMe.js
+++ src/logMe.js
@@ -18,9 +18,9 @@
 	};
 
 	var myIP = "66.130.40.67"; //http://www.whatismyip.com/
-	var remoteArray = [];
-	var remoteDelay = 2000;
-	var remoteTimeout = null;
+	var logArray   = [];
+	var logDelay   = 1000;
+	var logTimeout = null;
 
 	var error = false;
 
@@ -135,27 +135,27 @@
 			var $logger = $('#log-me');
 			if($logger.length == 0) {
 				var DOM = '' +
-					'<div id="log-me">' +
-					'<div class="log-me-header">' +
+					'<div id="log-me" class="box-sizing">' +
+					'<div class="log-me-header box-sizing">' +
 					'Logs' +
-					'<a class="log-me-btn log-me-drag" href="#"><i class="fa fa-arrows"></i></a>' +
-					'<a class="log-me-btn log-me-collapse hide" href="#"><i class="fa fa-compress"></i></a>' +
-					'<a class="log-me-btn log-me-full" href="#"><i class="fa fa-expand"></i></a>' +
 					'<a class="log-me-btn log-me-close" href="#"><i class="fa fa-times"></i></a>' +
+					'<a class="log-me-btn log-me-full" href="#"><i class="fa fa-expand"></i></a>' +
+					'<a class="log-me-btn log-me-collapse hide" href="#"><i class="fa fa-compress"></i></a>' +
+					'<a class="log-me-btn log-me-hide" href="#"><i class="fa fa-minus"></i></a>' +
+					'<a class="log-me-btn log-me-show hide" href="#"><i class="fa fa-plus"></i></a>' +
+					'<a class="log-me-btn log-me-drag" href="#"><i class="fa fa-arrows"></i></a>' +
 					'</div>' +
-					'<div class="log-me-ctn"></div>' +
+					'<div class="log-me-ctn box-sizing"></div>' +
 					'</div>';
 
 				var CSS = '' +
 					'<style id="log-me-styles" type="text/css">' +
 					'#log-me {position:fixed; top:0; left:0; background-color:black; color:white; width:160px; z-index:99999; border:1px solid white; font-family:"arial"; letter-spacing: -1px;} ' +
-					'#log-me > .log-me-header {position:relative; background-color:black; line-height:28px; font-size:14px; padding:0 5px}' +
-					'#log-me > .log-me-header .log-me-btn {position:absolute; top:50%; margin-top:-7px; right:5px; display:"block"; height:14px; width:14px; font-size:10px; line-height: 14px; color:#ffffff; text-align:center; background-color:grey;}' +
-					'#log-me > .log-me-header .log-me-btn i {margin-top:1px;}' +
-					'#log-me > .log-me-header .log-me-btn.log-me-full {right:20px;}' +
-					'#log-me > .log-me-header .log-me-btn.log-me-collapse {right:20px;}' +
-					'#log-me > .log-me-header .log-me-btn.log-me-drag {right:35px;}' +
-					'#log-me > .log-me-ctn {font-size:12px; line-height:14px; max-height:240px; overflow:auto;}' +
+					'#log-me > .box-sizing {-moz-box-sizing:border-box; -webkit-box-sizing:border-box; box-sizing:border-box;} ' +
+					'#log-me > .log-me-header {position:absolute; background-color:black; line-height:28px; font-size:14px; padding:0 5px; width:100%; clear:both;}' +
+					'#log-me > .log-me-header .log-me-btn {margin-top:7px; margin-left:3px; display:"block"; float:right; height:14px; width:14px; font-size:10px; line-height: 14px; color:#ffffff; text-align:center; background-color:grey;}' +
+					'#log-me > .log-me-header .log-me-btn.hide {display:none;}' +
+					'#log-me > .log-me-ctn {font-size:12px; line-height:14px; max-height:240px; overflow:auto; -webkit-overflow-scrolling:touch; width:100%; padding-top:28px;}' +
 					'#log-me > .log-me-ctn .log-me-row {padding:5px;}' +
 					'#log-me > .log-me-ctn .log-me-row:nth-child(even) {background-color:#1e1e1e;}' +
 					'#log-me > .log-me-ctn .log-me-row:nth-child(odd) {background-color:#313131;}' +
@@ -165,34 +165,66 @@
 					'</style>';
 
 				$('body').prepend(DOM);
-				// font typo added;
-				$('head').append('<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">');
 				$('head').append(CSS);
+				$('head').append('<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet" />');
 
 				$logger = $('#log-me');
 			}
+			privateMethods.addLoggerEvent.call(this, $logger);
 			return $logger;
 		},
+		addLoggerEvent: function($logger) {
+			var scope = this;
+			$logger.on('click', '.log-me-btn.log-me-hide', function(e){
+				$logger.find('.log-me-ctn').css({display:'none'});
+				$logger.find('.log-me-btn.log-me-hide').addClass('hide');
+				$logger.find('.log-me-btn.log-me-show').removeClass('hide');
+			});
+			$logger.on('click', '.log-me-btn.log-me-show', function(e){
+				$logger.find('.log-me-ctn').css({display:''});
+				$logger.find('.log-me-btn.log-me-hide').removeClass('hide');
+				$logger.find('.log-me-btn.log-me-show').addClass('hide');
+			});
+			$logger.on('click', '.log-me-btn.log-me-close', function(e){
+				scope.disableLog();
+			});
+			$logger.on('click', '.log-me-btn.log-me-full', function(e){
+				$logger.css({width:'100%', height:'100%'});
+				$logger.find('.log-me-ctn').css({display:'', maxHeight:'100%'});
+				$logger.find('.log-me-btn.log-me-full').addClass('hide');
+				$logger.find('.log-me-btn.log-me-collapse').removeClass('hide');
+				$logger.find('.log-me-btn.log-me-hide').addClass('hide');
+				$logger.find('.log-me-btn.log-me-show').addClass('hide');
+			});
+			$logger.on('click', '.log-me-btn.log-me-collapse', function(e){
+				$logger.css({width:'', height:''});
+				$logger.find('.log-me-ctn').css({maxHeight:''});
+				$logger.find('.log-me-btn.log-me-full').removeClass('hide');
+				$logger.find('.log-me-btn.log-me-collapse').addClass('hide');
+				$logger.find('.log-me-btn.log-me-hide').removeClass('hide');
+			});
+		},
 		mobileLog: function(log) {
-			var $logger = privateMethods.getLogger.call(this);
-			this.debug_count ++;
-
 			var formated_log = log;
-			if (typeof message === "object") {
+			if (typeof formated_log === "object") {
 				formated_log = JSON.stringify(formated_log);
 			}
-
-			var rowHtml = ""+
-				"<div class='log-me-row'>" +
-				"<div class='log-me-row-int'>" + this.debug_count + " :&nbsp;</div>" +
-				"<div class='log-me-row-output'>" + formated_log + "</div>" +
-				"<div class='log-me-row-clear'></div>" +
-				"</div>";
-
+			logArray.push(formated_log);
+			if(logTimeout){clearTimeout(logTimeout);}
+			logTimeout = setTimeout(Me.help.proxy(privateMethods.mobileSend, this), logDelay);
+		},
+		mobileSend: function() {
+			var scope = this;
+			var logsRequest = logArray;
+			logArray = [];
+			var $logger = privateMethods.getLogger.call(this);
+			var html = "";
+			$.each(logsRequest, function(index, log){
+				html += privateMethods.mobileAdd.call(scope, log);
+			});
 
 			var $logger_log_ctn = $logger.find('.log-me-ctn');
-			$logger_log_ctn.append(rowHtml);
-
+			$logger_log_ctn.append(html);
 			if (this.debug_height > 288) {
 				if($logger_log_ctn.scrollTop() == this.debug_height - 288 || $logger_log_ctn.scrollTop() == 0) {
 					$logger_log_ctn.scrollTop($logger_log_ctn[0].scrollHeight);
@@ -200,19 +232,27 @@
 			}
 			this.debug_height = $logger_log_ctn[0].scrollHeight;
 		},
+		mobileAdd: function(log) {
+			this.debug_count ++;
+			var html = "";
+			html += "<div class='log-me-row'><div class='log-me-row-int'>" + this.debug_count + " :&nbsp;</div><div class='log-me-row-output'>";
+			html += log;
+			html += "</div><div class='log-me-row-clear'></div></div>";
+			return html;
+		},
 		remoteLog: function(log) {
 			var formated_log = log;
-			if (typeof message === "object") {
+			if (typeof formated_log === "object") {
 				formated_log = JSON.stringify(formated_log);
 			}
-			remoteArray.push(formated_log);
-			if(remoteTimeout){clearTimeout(remoteTimeout);}
-			remoteTimeout = setTimeout(Me.help.proxy(privateMethods.remoteSend, this), remoteDelay);
+			logArray.push(formated_log);
+			if(logTimeout){clearTimeout(logTimeout);}
+			logTimeout = setTimeout(Me.help.proxy(privateMethods.remoteSend, this), logDelay);
 
 		},
 		remoteSend: function() {
-			var logsRequest = remoteArray;
-			remoteArray = [];
+			var logsRequest = logArray;
+			logArray = [];
 			var view = this;
 			$.ajax({
 				method:'POST',
